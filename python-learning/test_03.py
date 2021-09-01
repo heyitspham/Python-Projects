9f2a67a50512d8740a94e8f6129cb6433c850224
heyitspham <viquph@gmail.com>
2021-08-31 23:42:59 -0400

    File for testing functions from internet.

---
+++ python-learning/test_03.py
@@ -0,0 +1,6 @@
+import random
+def getColor( ):
+    lstColors = ['Red','Yellow','Blue','Green','Purple','Pink','White'   ,'Black']
+    return lstColors        
+color = random.choice(getColor())
+print(color)
